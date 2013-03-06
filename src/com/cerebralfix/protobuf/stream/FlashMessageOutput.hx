c71e907f385851316e2c0d826b23cd934a83dd31
David Alexander <david@cerebralfix.com>
2013-03-06 22:45:08 +1300

    Added FlashMessageOutput.

---
+++ src/com/cerebralfix/protobuf/stream/FlashMessageOutput.hx
@@ -0,0 +1,42 @@
+// This file is part of Protobuf-Haxe.
+// 
+// Protobuf-Haxe is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Protobuf-Haxe is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with Protobuf-Haxe.  If not, see <http://www.gnu.org/licenses/>.
+
+package com.cerebralfix.protobuf.stream;
+
+import haxe.io.Bytes;
+import haxe.io.BytesOutput;
+import flash.utils.IDataOutput;
+
+class FlashMessageOutput
+{
+	private var _output:IDataOutput;
+
+	public function new(output:IDataOutput):Void
+	{
+		_output = output;
+	}
+
+	// TODO: Support automatically packaging this into a union message.
+	public function writeMessage(message:Message):Void
+	{
+		var bytesOutput:BytesOutput = new BytesOutput();
+		message.writeMessageFields(bytesOutput);
+
+		var bytes:Bytes = bytesOutput.getBytes();
+
+		_output.writeInt(bytes.length);
+		_output.writeBytes(bytes.getData());
+	}
+}
