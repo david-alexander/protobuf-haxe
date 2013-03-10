da244b032e73cb28366f78e1e26b0b8666f041a8
David Alexander <david@cerebralfix.com>
2013-03-10 22:08:07 +1300

    Added the Proto file for the Chat Client sample, and modified the build script to run ProtoC over it.

--- src/build_flash.bat
+++ src/build_flash.bat
@@ -1,1 +1,2 @@
-haxe -swf ..\build\Main.swf -debug -main com.cerebralfix.protobuf.test.Main
+..\test\protoc --haxe_out=. --proto_path=..\test ..\test\ChatClientTest.proto
+haxe -swf ..\build\Main.swf -debug -main com.cerebralfix.protobuf.test.Main -xml ..\build\flash.xml
